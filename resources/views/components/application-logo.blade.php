1698dfe9b61a96e1b866a068a962f8f8212d1642
DominykMcNamara <dominyksmith@gmail.com>
2023-10-20 17:40:37 -0400

    tidy up

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="text-gray-800 font-semibold">
-    <span class="text-yellow-500 text-3xl">&lt;YELO&gt;</span>
+    <span class="text-yellow-500 text-3xl">&lt;DOM&gt;</span>Code
 </div>
 
